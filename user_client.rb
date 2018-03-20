650c7cd746c20864e835bc481b1b3da8b3809e31
Tom Willkens <tom.willkens@gmail.com>
2018-03-20 17:16:54 -0400

    reconfigure

--- user_client.rb
+++ user_client.rb
@@ -6,29 +6,17 @@ class UserClient
 
   def initialize name
     @name = name
-    @conversation = {}
+    @log = []
   end
 
-#  def join_game
-#    response = RestClient.post 'http://localhost:8080/join', :data => {name: @name}.to_json, :accept => :json
-#    puts JSON.parse(response,:symbolize_names => true)
-#  end
-#
-#  def get_cards
-#    response = RestClient.get 'http://localhost:8080/cards', {:params => {:name => @name}}
-#    puts response
-#  end
-#
-#  def deal
-#    response = RestClient.post 'http://localhost:8080/deal', :data =>{}.to_json, :accept => :json
-#    puts response
-#  end
-
   def get_hello
-    response = RestClient.get 'https://cryptic-anchorage-60369.herokuapp.com/hello', {}
+    response = RestClient.get 'enter url here', {}
     resp_hash = JSON.parse(response)
     key = Time.now.to_s
-    @conversation[key] = resp_hash['hello']
-    puts @conversation.inspect
+    @log.push({key: resp_hash['hello']})
+    if @log.length > 50
+      log.shift
+    end
+    puts @log.inspect
   end
 end
