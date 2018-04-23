7db0a88f37a1fb3a198eea644c3901e31f8db945
Alex Hu <alexhubnds@icloud.com>
2018-04-23 19:54:12 -0400

    ss

--- test_interface/test_interface.rb
+++ test_interface/test_interface.rb
@@ -178,7 +178,7 @@ end
 
 def load_all_users_from_seed(limit)
   result = []
-  File.open(SEED_USERS_DIR, 'r').each do |line|
+  File.open('./seeds/users.csv', 'r').each do |line|
     break if limit <= 0
     str = line.split(',')
     uid = Integer(str[0]) # ID provided in seed, useless for our implementation for now
@@ -194,7 +194,7 @@ def load_all_users_from_seed(limit)
 end
 
 def load_all_follows_from_tweet(limit)
-   File.open('./seeds/followsMINI.csv', 'r').each do |line|
+   File.open('./seeds/follows.csv', 'r').each do |line|
     break if limit <= 0
     str = line.split(',')
     id1 = Integer(str[0]) # ID provided in seed, useless for our implementation for now
@@ -206,7 +206,7 @@ def load_all_follows_from_tweet(limit)
 end
 
 def load_all_tweets_from_tweet(limit)
-  File.open('./seeds/tweetsMINI.csv', 'r').each do |line|
+  File.open('./seeds/tweets.csv', 'r').each do |line|
     break if limit <= 0 # enforce a limit if there is one
     str = line.split(',')
     id = str[0].to_i
